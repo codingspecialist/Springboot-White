916c52e174e283a3a52d7e57845c2d4af478da46
ssar <ssar@nate.com>
2022-08-29 16:14:40 +0900

    컨트롤러 데이터 받기 고급 (json, x-www-머시기)

---
+++ src/test/java/site/metacoding/white/ParseTest.java
@@ -0,0 +1,33 @@
+package site.metacoding.white;
+
+import org.junit.jupiter.api.Test;
+
+import site.metacoding.white.domain.Third;
+
+public class ParseTest {
+
+	@Test
+	public void parse_test() {
+		String data = "title=spring&content=enjoyspring&id=1";
+		
+		String[] p1 = data.split("&");
+		System.out.println(p1.length);
+		
+		System.out.println(p1[0]);
+		System.out.println(p1[1]);
+		System.out.println(p1[2]);
+		
+		Integer id =  Integer.parseInt(p1[2].split("=")[1]);
+		String title = p1[0].split("=")[1];
+		String content = p1[1].split("=")[1];
+				
+		Third third = new Third();
+		third.setId(id);
+		third.setTitle(title);
+		third.setContent(content);
+		
+		System.out.println(third.getId());
+		System.out.println(third.getTitle());
+		System.out.println(third.getContent());
+	}
+}
